1d10c1331176d7648239a3bd1a7bee20db60a41a
shamskhodary <shams.khodary@gmail.com>
2023-07-19 16:27:45 +0300

    Handled quill editor onchange and autosaving with new fonts and buttons(do, undo)

--- client/src/componenets/DocDetails.jsx
+++ client/src/componenets/DocDetails.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "../styles/document-page.css";
 import LogoWriter from "./LogoWriter";
-import { FilePdfFilled } from "@ant-design/icons";
+import { CheckCircleTwoTone, FilePdfFilled, SyncOutlined } from "@ant-design/icons";
 import * as moment from "moment";
 
-const DocDetails = ({data}) => {
+const DocDetails = ({data, saved}) => {
   return (
     <div className="doc-details" key={data.id}>
       <LogoWriter />
+      
       <h1>
-        {data.title} <sub className="opened_at"> Last opened: {moment(data.last_opened).fromNow()}</sub>
+        {data.title} <sub className="opened_at"> Last opened: {moment(data.last_opened).fromNow()} {saved ? <sub>     <CheckCircleTwoTone /> saved</sub>: <sub><SyncOutlined spin /> saving...</sub>} </sub>
+        
       </h1>
 
       <ul>
